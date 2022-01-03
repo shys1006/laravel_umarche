f4942569b3fd0145a44cf603f7c20d82ad3fbe23
shys1006 <konasho261006@icloud.com>
2022-01-03 20:30:29 +0900

    検索フォーム カテゴリー

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -114,4 +114,14 @@ class Product extends Model
 
     }
 
+    public function scopeSelectCategory($query, $categoryId)
+    {
+        if($categoryId !== '0')
+        {
+            return $query->where('secondary_category_id', $categoryId);
+        } else {
+            return ;
+        }
+    }
+
 }
